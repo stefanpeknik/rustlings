cfda2f2020fe44c3eb0cc21e6eb2ac2b6dd7fc1d
Stefan Peknik <stfn.peknik@gmail.com>
2024-09-26 23:05:27 +0200

    10 to 13 done

--- exercises/11_hashmaps/hashmaps1.rs
+++ exercises/11_hashmaps/hashmaps1.rs
@@ -9,11 +9,16 @@ use std::collections::HashMap;
 fn fruit_basket() -> HashMap<String, u32> {
     // TODO: Declare the hash map.
     // let mut basket =
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket.
+    basket.insert(String::from("banan"), 2);
+    basket.insert(String::from("bana"), 2);
+    basket.insert(String::from("ban"), 2);
+    basket.insert(String::from("ba"), 2);
 
     basket
 }
